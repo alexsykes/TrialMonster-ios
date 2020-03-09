91370d53c018968111ea95e51cd2eae8cb851f93
Alex <alex@alexsykes.net>
2020-03-09 15:52:36 +0000

    Extra ResultTableCell views added

---
+++ TrialMonster/ResultTableHeaderCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultTableHeaderCell.swift
+//  TrialMonster
+//
+//  Created by Alex Sykes on 09/03/2020.
+//  Copyright © 2020 Alex Sykes. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
