da3ed0ca94f984c529f2c1dc34e4e3e6573c5b59
Alex Sykes <alex@alexsykes.net>
2020-03-28 13:02:31 +0000

    Update

--- TrialMonster/ResultDetailViewController.swift
+++ TrialMonster/ResultDetailViewController.swift
@@ -12,12 +12,28 @@ class ResultDetailViewController: UIViewController {
     
     var result: Result?
     var trial: Trial?
-
+    @IBOutlet var trialLabel: UILabel!
+    @IBOutlet var bikeLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         // Do any additional setup after loading the view.
+        let title = result!.name
+        super.title = title
+
+        trialLabel.text = trial?.name
+        bikeLabel.text = "Machine: " + result!.machine
+        courseLabel.text = "Course: " + result!.course
+        if (result?.classs != "") {
+        classLabel.text = "Class: " + result!.classs
+        } else {
+            classLabel.text = ""
+        }
+        numberLabel.text = "Rider: " + result!.rider
     }
     
 
